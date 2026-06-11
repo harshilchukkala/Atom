c09975f54362d316ec1f9acf109e865155b9dbd5
harshilchukkala <harshilchukkala@gmail.com>
2026-06-11 21:06:34 +0530

    taking keyboard input for multiple options of atoms

--- main.cpp
+++ main.cpp
@@ -14,8 +14,21 @@ int main() {
         ClearBackground(BLACK);
         if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             Vector2 mouse=GetMousePosition();
-            Atoms.push_back(Atom(mouse.x,mouse.y,"H",WHITE,30));
-            Atoms.back().addshell(120,1);
+            if(IsKeyDown(KEY_H)) {
+                Atoms.push_back(Atom(mouse.x,mouse.y,"H",WHITE,30));
+                Atoms.back().addshell(120,1);
+            }
+            if(IsKeyDown(KEY_L)) {
+                Atoms.push_back(Atom(mouse.x,mouse.y,"Li",WHITE,30));
+                Atoms.back().addshell(120,2);
+                Atoms.back().addshell(160,1);
+            }
+            if(IsKeyDown(KEY_N)) {
+                Atoms.push_back(Atom(mouse.x,mouse.y,"Na",WHITE,30));
+                Atoms.back().addshell(120,1);
+                Atoms.back().addshell(160,8);
+                Atoms.back().addshell(200,1);
+            }
         }
         for(int i=0;i<Atoms.size();i++) {
             Atoms.at(i).draw();
